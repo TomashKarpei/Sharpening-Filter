3f74fd767c7c5435242d52e2da030c94455a4a3f
TomashKarpei <80202776+TomashKarpei@users.noreply.github.com>
2021-05-10 22:02:53 +0200

    Add files via upload

---
+++ Lab8_Filtr/src/filtr_max.java
@@ -0,0 +1,94 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import javax.imageio.ImageIO;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.util.Arrays;
+import javax.imageio.*;
+
+public class filtr_max {
+
+ BufferedImage image;
+ int width;
+ int height;
+
+ public filtr_max() {
+
+ try {
+ //odczyt obrazu z pliku
+
+ File input = new File("Ratusz_Bia³ystok_rok_2013.jpg");
+
+ image = ImageIO.read(input);
+ width = image.getWidth();
+ height = image.getHeight();
+ int p = 8;
+ //odczyt pixeli obrazu w dwóch pêtlach po kolumnach i wierszach
+
+ for(int i=1; i<height-1; i++){
+ for(int j=1; j<width-1; j++){
+	 int[] pomoc_r = new int[] {0,0,0,0,0,0,0,0,0};
+	 int[] pomoc_g = new int[] {0,0,0,0,0,0,0,0,0};
+	 int[] pomoc_b = new int[] {0,0,0,0,0,0,0,0,0};
+	 int g = 0;
+	 
+   for (int k=-1;k<=1;k++) {
+     for (int l=-1; l<=1;l++) {
+    	 
+       //odczyt sk³adowych koloru RGB
+
+        Color c = new Color(image.getRGB(j+k, i+l));
+
+        int red = (int)(c.getRed());
+        int green = (int)(c.getGreen());
+        int blue = (int)(c.getBlue());
+
+       pomoc_r[g] = red;
+       pomoc_g[g] = green;
+       pomoc_b[g] = blue;
+       g++;
+   }
+   }
+   //filtrowanie od najmiejszej do najwiêkszej wartoœci kolorów  
+   Arrays.sort(pomoc_g);
+   Arrays.sort(pomoc_b);
+
+  //System.out.println("\n" + pomoc_r[p] +" "+ pomoc_g[p] + " " + pomoc_b[p]);
+
+
+ if (pomoc_r[p] >=0 && pomoc_r[p] <=255) {}
+ else pomoc_r[p] = 0;
+
+ if (pomoc_g[p] >=0 && pomoc_g[p] <=255) {}
+ else pomoc_g[p] = 0;
+
+ if (pomoc_b[p] >=0 && pomoc_b[p] <=255) {}
+ else pomoc_b[p] = 0;
+
+   
+
+ Color newColor = new Color(pomoc_r[p], pomoc_g[p], pomoc_b[p]);
+ image.setRGB(j,i,newColor.getRGB());
+
+
+
+ } //koniec dwóch pêtli po kolumnach i wierszach obrazu
+ }
+
+ //zapis do pliku zmodyfikowanego obrazu
+
+ File ouptut = new File("filtr_min.jpg");
+ ImageIO.write(image, "jpg", ouptut);
+
+ } catch (Exception e) {}
+ }
+
+
+
+ static public void main(String args[]) throws Exception
+ {
+ filtr_max obj = new filtr_max();
+ }
+}
---
+++ Lab8_Filtr/src/filtr_prewitta2.java
@@ -0,0 +1,97 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+
+public class filtr_prewitta2 {
+
+ BufferedImage image;
+ int width;
+ int height;
+
+ public filtr_prewitta2() {
+
+ try {
+ //odczyt obrazu z pliku
+
+ File input = new File("Ratusz_Bia³ystok_rok_2013.jpg");
+
+ image = ImageIO.read(input);
+ width = image.getWidth();
+ height = image.getHeight();
+
+ int[][] M = new int[][] { {1,0,-1}, {1,0,-1}, {1,0,-1}};
+
+ int pomoc_r, pomoc_g, pomoc_b;
+
+ //odczyt pixeli obrazu w dwóch pêtlach po kolumnach i wierszach
+
+ for(int i=1; i<height-1; i++){
+ for(int j=1; j<width-1; j++){
+   
+   pomoc_r = 0;
+   pomoc_g = 0;
+   pomoc_b = 0;
+
+
+   for (int k=-1;k<=1;k++) {
+     for (int l=-1; l<=1;l++) {
+
+       //odczyt sk³adowych koloru RGB
+
+        Color c = new Color(image.getRGB(j+k, i+l));
+
+        int red = (int)(c.getRed());
+        int green = (int)(c.getGreen());
+        int blue = (int)(c.getBlue());
+
+       pomoc_r += red  * M[k+1][l+1];
+       pomoc_g += green * M[k+1][l+1];
+       pomoc_b += blue * M[k+1][l+1];
+
+   }
+   }
+
+   pomoc_r = (int) Math.abs(pomoc_r);
+   pomoc_g = (int) Math.abs(pomoc_g);
+   pomoc_b = (int) Math.abs(pomoc_b);
+
+
+ //System.out.println("\n" + pomoc_r +" "+ pomoc_g + " " + pomoc_b );
+
+
+ if (pomoc_r>=0 && pomoc_r <=255) {}
+ else pomoc_r = 0;
+
+ if (pomoc_g>=0 && pomoc_g <=255) {}
+ else pomoc_g = 0;
+
+ if (pomoc_b>=0 && pomoc_b <=255) {}
+ else pomoc_b = 0;
+
+   
+
+ Color newColor = new Color(pomoc_r, pomoc_g, pomoc_b );
+ image.setRGB(j,i,newColor.getRGB());
+
+
+
+ } //koniec dwóch pêtli po kolumnach i wierszach obrazu
+ }
+
+ //zapis do pliku zmodyfikowanego obrazu
+
+ File ouptut = new File("filtr_prewitta2.jpg");
+ ImageIO.write(image, "jpg", ouptut);
+
+ } catch (Exception e) {}
+ }
+
+
+
+ static public void main(String args[]) throws Exception
+ {
+ filtr_prewitta2 obj = new filtr_prewitta2();
+ }
+}
